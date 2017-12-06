3a8398bd31aedda7d256ccfeaa5ec65e5a3575a1
huiye <1011948200@qq.com>
2017-12-06 14:46:31 +0800

    login2

--- src/main/java/cm/hm/controller/Login.java
+++ src/main/java/cm/hm/controller/Login.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by HUIYE on 2017/12/6.
  */
@@ -16,13 +19,18 @@ public class Login {
     private UserMapper userMapper;
 
     @RequestMapping("/login.action")
-    public String login(User user){
+    public String login(User user, HttpSession Session){
         System.out.println(user);
         User user1 = userMapper.selectByName(user);
         if(user1!=null){
+            Session.setAttribute("u_name",user.getU_name());
             return "menu";
         }
         return "login";
 
+    }
+    @RequestMapping("test.action")
+    public String test(){
+        return "menu";
     }
 }
---
+++ src/main/java/cm/hm/mapper/test.java
@@ -0,0 +1,13 @@
+package cm.hm.mapper;
+
+import cm.hm.dao.Singer;
+
+/**
+ * Created by HUIYE on 2017/12/6.
+ */
+public class test implements  SingerMapper {
+    @Override
+    public Singer selectByName(String name) {
+        return null;
+    }
+}
